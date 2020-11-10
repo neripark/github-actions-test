837f78b43b9bcd105269cd4554374231572c3d59
neripark <neripark@gmail.com>
2020-11-11 01:04:17 +0900

    split one line

--- git.sh
+++ git.sh
@@ -13,11 +13,22 @@ git config pull.rebase false
 # echo '## git push origin/master:origin/feature/dummy ##'
 # git push origin origin/master:origin/feature/dummy
 echo '*** merge to feature/dummy ***'
-git pull
+# git pull
+
+echo '*** git checkout feature/dummy ***'
 git checkout feature/dummy
 
 echo '*** git log ***'
 git log -n 1
+
+echo '*** git branch ***'
+git branch
+
+echo '*** git pull ***'
 git pull
+
+echo '*** git merge master ***'
 git merge master
+
+echo '*** git push ***'
 git push
