76a2f646ada806ca42995d958c3f256f2c886c3c
neripark <neripark@gmail.com>
2020-11-11 00:50:14 +0900

    add manipulate git config

--- git.sh
+++ git.sh
@@ -4,8 +4,12 @@ git fetch
 echo '*** git branch ***'
 git branch
 
-echo '## git push origin/master:origin/feature/dummy ##'
+echo '## git config pull.rebase false ##'
+git config pull.rebase false
+
+# echo '## git push origin/master:origin/feature/dummy ##'
 # git push origin origin/master:origin/feature/dummy
+echo '*** merge to feature/dummy ***'
 git pull
 git checkout feature/dummy
 git pull
