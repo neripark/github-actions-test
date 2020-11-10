6e3d07231625d8400a805019fbf6ea5cb0fa9134
neripark <neripark@gmail.com>
2020-11-11 00:43:53 +0900

    add pull

--- git.sh
+++ git.sh
@@ -6,6 +6,7 @@ git branch
 
 echo '## git push origin/master:origin/feature/dummy ##'
 # git push origin origin/master:origin/feature/dummy
+git pull
 git checkout feature/dummy
 git pull
 git merge master
