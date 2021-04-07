ca52a8b5ab9be83038b2630b2eb5c3463ab8a3b3
neripark <neripark@gmail.com>
2021-04-07 18:34:10 +0900

    add debug code to ci

--- git.sh
+++ git.sh
@@ -10,25 +10,25 @@ git branch
 echo '*** git pull ***'
 git pull
 
-# echo '*** git log ***'
-# git log -n 1
+echo '*** git log ***'
+git log -n 1
 
 # echo '## git config pull.rebase false ##'
 # git config pull.rebase false
 
 # echo '## git push origin/master:origin/feature/dummy ##'
 # git push origin origin/master:origin/feature/dummy
-echo '*** merge to feature/dummy ***'
+# echo '*** merge to feature/dummy ***'
 # git pull
 
 echo '*** git checkout feature/dummy ***'
 git checkout feature/dummy
 
-# echo '*** git log ***'
-# git log -n 1
+echo '*** git log ***'
+git log -n 1
 
-# echo '*** git branch ***'
-# git branch
+echo '*** git branch ***'
+git branch
 
 echo '*** git merge master ***'
 git merge master
